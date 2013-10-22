51e476c55ee0e6534f66819f8a05dfa7119c66cf
Afshin Mokhtari <afshinator@hotmail.com>
2013-10-22 01:24:47 -0700

    Fixed picture bug, added rotation!

--- SketchPad/sketchpad.js
+++ SketchPad/sketchpad.js
@@ -6,20 +6,20 @@
 
 
 $(function() {
-	var dimension = 9;													// default grid size
-	var boxSize = Math.floor( ( 960 - ( dimension * 2) ) / dimension ); // pixel width of each box given # of boxes per row/column
-	var container$ = $('#container');				// Since we need to keep referencing this, cache the selection here once and for all.
-	var boxes$;
+	var dimension = 16;								// default box size
+	var boxSize = Math.floor( 960 / dimension );	// pixels per box
 
-	var catCategories = ['hats', 'space', 'funny', 'sunglasses', 'boxes', 'caturday'];		// random cat images not working yet.
+	var container$ = $('#container');				// Since we need to keep referencing this, cache the selection here once and for all.
+	var boxes$;										// Will be selection of all boxes created, reset everytime a new grid is created 
+	var apiStr = 'src="http://thecatapi.com/api/images/get?&format=src&type=gif&size=small"';	// Cat images api
+
 
 	$(":button").click(function() {
-		var whichButton = parseInt($(this).attr("value"),10);		// Use tag from HTML to determine which button was pressed
-		dimension = prompt('How many boxes? (or RETURN for default)');
+		var whichButton = parseInt($(this).attr("value"),10);		// Use value tag from HTML for which button was pressed
+		dimension = prompt('How many boxes? RETURN for default)');
 
-		if ( dimension === "" || dimension < 1 || dimension > 128 ) {
-			console.log(dimension);
-			dimension = 9;
+		if ( isNaN(dimension)  || dimension < 1 || dimension > 128 ) {
+			dimension = ((whichButton === 5) ? 3 : 16);		// default for images is 3x3 instead of 16x16
 		}
 		new_grid(dimension);
 		handle(whichButton);
@@ -27,12 +27,12 @@ $(function() {
 
 
 	var handle = function(which) {
-		function get_random_color() {
+		function get_random_color() {	// obfuscation or beautifully terse?
 			return '#'+Math.floor(Math.random()*16777215).toString(16);
 		}
 
 		boxes$.hover(
-			function() {
+			function() {				// What to do on mouse inside a box
 				switch(which) {
 					case 1:									// Button 1 : color change
 						$(this).addClass('simpleColor');
@@ -47,27 +47,32 @@ $(function() {
 					case 4:									// Button 4: leave a trail!
 						$(this).fadeTo(0,0);					// fadeTo(duration, opacity)
 						$(this).mouseleave(function(){
-							$(this).fadeTo(800,1);
+							$(this).fadeTo(600,1);
 						});
 						break;
 					case 5:									// Button 5: Random cat picture!
-						var rnd = Math.random() * catCategories.length;
-				//		var apiStr = 'src="http://thecatapi.com/api/images/get?&category=' +  catCategories[rnd] + '&format=src&type=gif&size=small"';
-						var apiStr = 'src="http://thecatapi.com/api/images/get?&format=src&type=gif&size=small"';
-
-						$(this).append('<img width="' + boxSize + '" height="' + boxSize + '" ' + apiStr + ' >');
+						if ( $(this).html() === "" ) {		// If we havent hit this box before
+							$(this).append('<img width="' + boxSize + '" height="' + boxSize + '" ' + apiStr + ' >');
+							$(this).addClass('rotate15');
+						} else {	// apply class for img rotation when mouse over image
+							if (! $(this).hasClass('rotate15')) {
+								$(this).addClass('rotate15');
+							}
+						}
 						break;
 				}
-			} /* ,			// If you want to do something on mouse-out, put it here...
-			function() {
+			},
+			function() {	// 2nd parameter to hover(), stuff to do on mouse-out of a box
 				switch(which) {
 					case 1:
 						// $(this).removeClass('simpleColor');
 						break;
 					case 5:
 						// $(this).empty();
+						$(this).removeClass('rotate15');
+						break;
 				}
-			} */
+			}
 		);
 	};
 
@@ -75,18 +80,21 @@ $(function() {
 
 	var new_grid = function(size) {
 		var divsToInsert = [];
-		boxSize = Math.floor( ( 960 - ( size * 2) ) / size );
+		boxSize = Math.floor( 960 / size );
 
 		container$.html('');		// Empty out container. Would .empty() be faster or slower?
 
-		// Its faster to put all the html into an array, call join, and append once at the end rather than
-		// simply putting the append inside the for loop.
+		container$.css('width', boxSize * size);	// Make container the size of one row, avoid a whole bunch of other issues.
+		// Its faster to put all the html into an array, call join, and append once at the end,
+		// rather than simply putting the append inside the for loop.
 		for ( var i = 0 ; i < size * size; i += 1) {
 			divsToInsert[i] = "<div class='box' style='width:" + boxSize + "px; height:" + boxSize+"px;'></div>";
+			// In case you want to count the boxes in browser inspector, uncomment the line below, it'll put it in the data attribute
+			// divsToInsert[i] = "<div class='box' data='" + i + "' style='width:" + boxSize + "px; height:" + boxSize+"px;'></div>";
 		}
 
 		container$.append(divsToInsert.join(''));    // http://www.learningjquery.com/2009/03/43439-reasons-to-use-append-correctly
-		boxes$ = $(".box");			// cache the selection of all boxes creted for the hover event handlers
+		boxes$ = $(".box");			// cache the selection of all boxes created for the hover event handlers; slow!
 	};
 
 	// First time page is loaded, build a default dimension grid and use the first the button options
